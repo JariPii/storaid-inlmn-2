458e9708ed4fd7316963282421898f26f75acc3b
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2025-10-16 18:57:25 +0200

    Adds services section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AboutSection from '@/components/about/AboutSectio';
 import BrandsBar from '@/components/brands/BrandsBar';
 import HeroHome from '@/components/hero/HeroHome';
+import ServicesSection from '@/components/services/ServicesSection';
 import Section from '@/components/utils/Section';
 
 const HomePage = () => {
@@ -9,9 +10,9 @@ const HomePage = () => {
       <Section>
         <HeroHome />
       </Section>
-
       <AboutSection />
       <BrandsBar />
+      <ServicesSection />
     </div>
   );
 };
