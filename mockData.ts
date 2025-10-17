797f26e57b69ef393b348823f6cb6dcd9a67840b
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2025-10-17 16:54:20 +0200

    Changes remotePatterns for images

--- mockData.ts
+++ mockData.ts
@@ -15,7 +15,7 @@ export const testimonials: Testimonial[] = [
     // rating: '3',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipis cing elit. Ut libero lectus, porta nec turpis sit amet, lobortis fringilla ante.',
-    avatar: 'https://randomuser.me/api/portraits/men/32.jpg',
+    avatar: 'https://randomuser.me/api/portraits/med/men/32.jpg',
     name: 'Aiden Harvey',
     role: 'Customer',
   },
@@ -25,7 +25,7 @@ export const testimonials: Testimonial[] = [
     // rating: '5',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipis cing elit. Ut libero lectus, porta nec turpis sit amet, lobortis fringilla ante.',
-    avatar: 'https://randomuser.me/api/portraits/women/44.jpg',
+    avatar: 'https://randomuser.me/api/portraits/med/women/44.jpg',
     name: 'Sarah Geller',
     role: 'Customer',
   },
@@ -35,7 +35,7 @@ export const testimonials: Testimonial[] = [
     // rating: '3',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipis cing elit. Ut libero lectus, porta nec turpis sit amet, lobortis fringilla ante.',
-    avatar: 'https://randomuser.me/api/portraits/women/68.jpg',
+    avatar: 'https://randomuser.me/api/portraits/med/women/68.jpg',
     name: 'Olivia Harvey',
     role: 'Customer',
   },
