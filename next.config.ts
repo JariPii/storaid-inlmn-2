ce1e21c01428f5381666ef828000cc89534a58bf
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2025-10-17 16:20:44 +0200

    Tries to fix image permissons in vercel

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL('https://randomuser.me/api/portraits/*/*.jpg')],
+    remotePatterns: [new URL('https://randomuser.me/api/portraits/**/**.**')],
   },
 };
 
