154021d60d35f0cafe1bdb76ffa10f7f56fb172d
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2025-10-18 03:12:03 +0200

    Adds pricingpland card

---
+++ public/images/icons/CheckMark.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+const CheckMark = (props) => (
+  <svg
+    width='800px'
+    height='800px'
+    viewBox='0 0 76 76'
+    xmlns='http://www.w3.org/2000/svg'
+    xmlnsXlink='http://www.w3.org/1999/xlink'
+    baseProfile='full'
+    enableBackground='new 0 0 76.00 76.00'
+    xmlSpace='preserve'
+    {...props}
+  >
+    <path
+      fill='#000000'
+      fillOpacity={1}
+      strokeWidth={0.2}
+      strokeLinejoin='round'
+      d='M 23.7501,33.25L 34.8334,44.3333L 52.2499,22.1668L 56.9999,26.9168L 34.8334,53.8333L 19.0001,38L 23.7501,33.25 Z '
+    />
+  </svg>
+);
+export default CheckMark;
--- public/images/icons/icons.ts
+++ public/images/icons/icons.ts
@@ -1,6 +1,5 @@
-import padlock from '@/public/images/icons/padlock.svg';
-import moneybag from '@/public/images/icons/moneybag.svg';
-import broom from '@/public/images/icons/broom.svg';
-import clock from '@/public/images/icons/clock.svg';
-
-export { padlock, moneybag, broom, clock };
+export { default as padlock } from '@/public/images/icons/padlock.svg';
+export { default as moneybag } from '@/public/images/icons/moneybag.svg';
+export { default as broom } from '@/public/images/icons/broom.svg';
+export { default as clock } from '@/public/images/icons/clock.svg';
+export { default as check } from '@/public/images/icons/check.svg';
