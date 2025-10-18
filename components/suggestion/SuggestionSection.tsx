84a096e465bc6738a0cce634ac5eb3c17dcfd0d8
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2025-10-18 03:57:31 +0200

    Adds suggestion section

--- components/suggestion/SuggestionSection.tsx
+++ components/suggestion/SuggestionSection.tsx
@@ -7,7 +7,7 @@ const SuggestionSection = () => {
       <h2 className='text-[3.25rem] font-bold leading-[3.25rem] w-[40ch]'>
         Looking for Secure and Flexible Storage? Find Your Perfect Fit With Us.
       </h2>
-      <p className=' w-[128ch] leading-6'>
+      <p className=' w-[128ch] leading-6 tracking-wider'>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
         molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac
         blandit nisi non sodales augue. Phasellus eget elit gravida.
