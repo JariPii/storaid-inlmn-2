766e08f75da4497ec6aa74872a077701e8f6f98a
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2025-10-18 17:04:44 +0200

    Fixes hover background color and adds animation

--- components/blog/BlogCard.tsx
+++ components/blog/BlogCard.tsx
@@ -12,7 +12,7 @@ type BlogCardProps = {
 
 const BlogCard = ({ image, alt, date, title, content }: BlogCardProps) => {
   return (
-    <div className='flex flex-col gap-4 w-[26.875rem] bg-(--clr-secondary) p-6 rounded-[5px]'>
+    <div className='flex flex-col gap-4 w-[26.875rem] bg-(--clr-secondary) hover:bg-(--clr-color-4) hover:transition-colors hover:duration-500 group p-6 rounded-[5px]'>
       <Image
         src={image}
         alt={alt}
@@ -20,12 +20,18 @@ const BlogCard = ({ image, alt, date, title, content }: BlogCardProps) => {
         height={1000}
         className='rounded-[5px]'
       />
-      <div className='flex gap-2 text-(--clr-primary) items-baseline'>
-        <FaRegCalendar className='text-lg' />
-        <p className=' text-[14px] '>{date}</p>
+      <div className='flex gap-2 text-(--clr-primary)  items-baseline'>
+        <FaRegCalendar className='text-lg group-hover:text-(--clr-secondary)' />
+        <p className=' text-[14px] group-hover:text-(--clr-secondary)'>
+          {date}
+        </p>
       </div>
-      <h2 className='text-(--clr-primary) text-xl font-bold'>{title}</h2>
-      <p className='text-(--clr-primary)'>{content}</p>
+      <h2 className='text-(--clr-primary) group-hover:text-(--clr-secondary) text-xl font-bold'>
+        {title}
+      </h2>
+      <p className='text-(--clr-primary) group-hover:text-(--clr-secondary)'>
+        {content}
+      </p>
       <Link href='/' className='text-(--clr-accent) flex items-center gap-2'>
         Read more <FaArrowRight />
       </Link>
