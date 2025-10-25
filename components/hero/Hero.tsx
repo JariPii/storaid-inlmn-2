c8da65bb078ea91855f930a455bc86839caaea0f
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2025-10-25 03:51:27 +0200

    Adds reworked Hero component

---
+++ components/hero/Hero.tsx
@@ -0,0 +1,59 @@
+import Image, { StaticImageData } from 'next/image';
+import Section from '../utils/Section';
+import Button from '../buttons/Button';
+import Link from 'next/link';
+
+export type HeroProps = {
+  invitation?: string;
+  title: string;
+  description: string;
+  buttonText?: string;
+  buttonLink?: string;
+  imgSrc?: string | StaticImageData;
+  alt?: string;
+};
+
+const Hero = ({
+  invitation,
+  title,
+  description,
+  imgSrc,
+  alt,
+  buttonText,
+  buttonLink,
+}: HeroProps) => {
+  return (
+    <Section>
+      <div className='grid grid-cols-[60%_1fr] py-[10rem] grid-rows-[1fr] relative'>
+        <div className='flex items-start justify-center flex-col gap-4'>
+          {invitation && (
+            <h1 className='text-(--clr-accent) font-bold leading-6 text-2xl'>
+              {invitation}
+            </h1>
+          )}
+          <h2 className='text-[5rem] text-(--background) font-bold leading-20'>
+            {title}
+          </h2>
+          <p className='text-(--background) w-[70ch] leading-6'>
+            {description}
+          </p>
+          {buttonText && buttonLink && (
+            <Link href={`/${buttonLink}`}>
+              <Button hover='dark'>{buttonText}</Button>
+            </Link>
+          )}
+        </div>
+        {imgSrc && (
+          <Image
+            src={imgSrc}
+            alt={alt || 'hero image'}
+            className='h-[32rem] absolute bottom-0 right-0'
+            width={500}
+            height={500}
+          />
+        )}
+      </div>
+    </Section>
+  );
+};
+export default Hero;
---
+++ components/hero/heroContent.ts
@@ -0,0 +1,55 @@
+import { StaticImageData } from 'next/image';
+import image from '../../public/images/Rectangle2.svg';
+
+type HeroContent = {
+  invitation?: string;
+  title: string;
+  description: string;
+  showImage?: string;
+  buttonText?: string;
+  buttonLink?: string;
+  imgSrc?: string | StaticImageData;
+  image?: object;
+  alt?: string;
+};
+
+type HeroContentList = {
+  home: HeroContent;
+  about: HeroContent;
+  contact: HeroContent;
+  services: HeroContent;
+  booking: HeroContent;
+};
+
+export const heroContent: HeroContentList = {
+  home: {
+    invitation: 'Welcome to StorAid',
+    title: 'Space Simplified, Storage Perfected',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a sem magna. Etiam ac odio sit amet lorem fringilla soda les. Cras lobortis, libero at iaculis luctus, nisi ex pellentesque nisi, at euismod sem ipsum ac dolor.',
+    buttonText: 'Discover More',
+    buttonLink: 'about',
+    imgSrc: image,
+    alt: 'hero image',
+  },
+  about: {
+    title: 'About Us',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.',
+  },
+  contact: {
+    title: 'Contact Us',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.',
+  },
+  services: {
+    title: 'Our Services',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.',
+  },
+  booking: {
+    title: 'Book Storage now',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.',
+  },
+};
