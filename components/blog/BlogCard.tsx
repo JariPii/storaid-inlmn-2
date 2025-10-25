710f6084156e8f1dd1be5b9b6b0eb9fc4e056e16
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2025-10-25 18:15:11 +0200

    Adds sizing to active blogCard

--- components/blog/BlogCard.tsx
+++ components/blog/BlogCard.tsx
@@ -21,14 +21,12 @@ const BlogCard = ({
   isExpanded = false,
   onToggle,
 }: BlogCardProps) => {
-  // const [isExpanded, setIsExpanded] = useState<boolean>(false);
-
   return (
     <div
       className={cn(
         'flex flex-col gap-4 w-[26.875rem] bg-(--clr-secondary) hover:bg-(--clr-color-4) hover:transition-colors hover:duration-500 group p-6 rounded-[5px] hover:cursor-pointer h-[36rem]',
         {
-          'h-auto': isExpanded,
+          'h-auto bg-(--clr-color-4)': isExpanded,
         }
       )}
       onClick={() => onToggle?.(id)}
@@ -41,30 +39,55 @@ const BlogCard = ({
         className='rounded-[5px]'
       />
       <div className='flex gap-2 text-(--clr-primary)  items-baseline'>
-        <FaRegCalendar className='text-lg group-hover:text-(--clr-secondary)' />
-        <p className=' text-[14px] group-hover:text-(--clr-secondary)'>
+        <FaRegCalendar
+          className={cn('text-lg group-hover:text-(--clr-secondary)', {
+            isExpandedText: isExpanded,
+          })}
+        />
+        <p
+          className={cn(' text-[14px] group-hover:text-(--clr-secondary)', {
+            isExpandedText: isExpanded,
+          })}
+        >
           {created}
         </p>
       </div>
-      <h2 className='text-(--clr-primary) group-hover:text-(--clr-secondary) text-xl font-bold'>
+      <h2
+        className={cn(
+          'text-(--clr-primary) group-hover:text-(--clr-secondary) text-xl font-bold',
+          { isExpandedText: isExpanded }
+        )}
+      >
         {title}
       </h2>
       <p
         className={cn(
           'text-(--clr-primary) group-hover:text-(--clr-secondary) overflow-hidden transition-[max-height] duration-300',
+          { 'text-(--clr-secondary)': isExpanded },
           isExpanded ? 'line-clamp-none' : 'line-clamp-3'
         )}
       >
         {description}
       </p>
-      {isExpanded ? 'Read Less' : 'Read More'}
-      <FaArrowRight
-        className={cn(
-          'trasition-transform duration-500',
-          isExpanded ? 'rotate-180' : 'rotate-0'
-        )}
-      />
+      <div className='flex items-center gap-1.5'>
+        <p
+          className={cn('text-(--clr-accent)', {
+            isExpandedText: isExpanded,
+          })}
+        >
+          {isExpanded ? 'Read Less' : 'Read More'}
+        </p>
+        <FaArrowRight
+          className={cn(
+            'text-(--clr-accent)',
+            'trasition-all duration-500',
+            isExpanded ? 'rotate-180 text-(--clr-secondary)' : 'rotate-0'
+          )}
+        />
+      </div>
     </div>
   );
 };
 export default BlogCard;
+
+// TODO: Finish the blogcards text and so on!!!
