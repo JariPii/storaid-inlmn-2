f20574cc9d32d3c9750105e87f11ccf8e5ce1da7
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2025-11-07 16:41:39 +0100

    Adds custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+import { Button, NavButton } from '@/components/buttons/Buttons';
+import Section from '@/components/utils/Section';
+import Link from 'next/link';
+
+const ErrorPage = () => {
+  return (
+    <Section className='h-[80dvh] flex flex-col items-center justify-center gap-5'>
+      <p className='text-2xl text-(--clr-primary)'>Oops!</p>
+      <h2 className='text-[5rem] text-(--clr-accent) font-bold tracking-tight'>
+        Page Not Found
+      </h2>
+      <p className='text-(--clr-primary) w-[70ch] text-center leading-6'>
+        Sorry, the page you are looking for does not exist. It may have been
+        moved, removed altogether or is temporarily unavailable.
+      </p>
+      <NavButton href='/' />
+    </Section>
+  );
+};
+export default ErrorPage;
