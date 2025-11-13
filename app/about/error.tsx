6000b63c232b24c6050206f5a96632ef0a6f0c5b
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2025-11-13 22:59:35 +0100

    Adds error handling and images formatting

---
+++ app/about/error.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import BlogCardSkeleton from '@/components/global/BlogCardSkeleton';
+import Section from '@/components/global/Section';
+
+export default function ErrorComp({ error }: { error: Error }) {
+  return (
+    <div>
+      <div>
+        <Section variant='light' className='flex flex-col gap-4 py-20'>
+          <div className='bg-(--clr-secondary) p-2'>
+            <h3 className='text-2xl text-(--clr-accent) font-bold'>
+              Latest Blog and News
+            </h3>
+          </div>
+          <div className='grid grid-cols-2 gap-1'>
+            <div className='w-full'>
+              <h4 className='text-[3.25rem] font-bold leading-[52px]'>
+                Check Out Our Latest Blog and News Update
+              </h4>
+            </div>
+
+            <div className='flex items-center h-auto'>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                molestie nisl sed dui lacinia gravida. Nulla quis nulla leo.
+                Mauris ac blandit nisi non sodales augue. Phasellus eget elit
+                gravida.
+              </p>
+            </div>
+          </div>
+
+          <div className='flex flex-col items-center gap-6'>
+            <div className='flex gap-4'>
+              <h3 className='text-3xl text-red-400 font-bold'>
+                <span>{error.message}</span>
+              </h3>
+            </div>
+
+            <div className='flex gap-6'>
+              <BlogCardSkeleton />
+              <BlogCardSkeleton />
+              <BlogCardSkeleton />
+            </div>
+          </div>
+        </Section>
+      </div>
+    </div>
+  );
+}
